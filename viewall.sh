084a5b655ae07e24136ecab5910a1a39e1c8bd19
tg <tg@example.com>
2018-09-07 18:44:00 +0200

    Improve viewall script.

--- viewall.sh
+++ viewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in $(ls $@/*/*.{MRMM,3DSM}); do
+for f in $(find $1/ -name '*.MRMM' -type f; find $1/ -name '*.3DSM' -type f; find $1/ -name '*.SXMD' -type f); do
     echo $f
     ./3d $f
 done
