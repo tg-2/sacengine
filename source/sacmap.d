a30c3540929a28f715c7eab0439bd81dab88e18f
tg <tg@example.com>
2018-09-30 09:24:01 +0200

    Rewrite map renderer, add support for detail textures.

--- source/sacmap.d
+++ source/sacmap.d
@@ -3,113 +3,111 @@ import util;
 import hmap,tmap,txtr;
 import std.exception, std.string, std.algorithm, std.conv, std.range;
 import std.stdio, std.path, std.file;
+import std.typecons: tuple,Tuple;
 
-class SacMap{
-	Mesh mesh;
-	Texture texture;
-	Texture bump;
+class SacMap{ // TODO: make this an entity
+	Mesh[] meshes;
+	Texture[] textures;
+	Texture[] bump;
+	ubyte[] dti;
 
 	this(string filename){
 		enforce(filename.endsWith(".HMAP"));
 		auto hmap=loadHMap(filename);
 		auto tmap=loadTMap(filename[0..$-".HMAP".length]~".TMAP");
-		mesh=createMesh(hmap,tmap);
+		meshes=createMeshes(hmap,tmap);
+		auto land="extracted/prsc/prsc.WAD!/prsc.LAND";
+		dti=loadDTIndex(land).dts;
+		static Texture makeTexture(SuperImage i){
+			auto texture=New!Texture(null); // TODO: set owner
+			texture.image=i;
+			texture.createFromImage(texture.image);
+			return texture;
+		}
+		static SuperImage addDetail(ref SuperImage img,ref SuperImage dt){
+			auto r=image(dt.width,dt.height);
+			foreach(j;0..256){
+				foreach(i;0..256){
+					auto cur=img[j/4,i/4];
+					auto up=j/4?img[j/4-1,i/4]:cur;
+					auto ri=i/4<63?img[j/4,i/4+1]:cur;
+					auto lo=j/4<63?img[j/4+1,i/4]:cur;
+					auto le=i/4<63?img[j/4-1,i/4]:cur;
+					auto col=(cur+up+ri+lo+le)/5;
+					auto det=dt[j,i];
+					r[j,i]=0.5*col+0.5*Color4f(col.r*det.r,col.g*det.g,col.b*det.b);
+				}
+			}
+			return r;
+		}
+		auto mapts=loadMAPTs(land);
+		auto bumps=loadDTs(land);
+		textures=iota(256).map!(i=>addDetail(mapts[i],bumps[dti[i]])).map!makeTexture.array;
+		bump=bumps.map!makeTexture.array;
+		//textures=loadMAPTs(land).map!makeTexture.array;
 	}
 
 	void createEntities(Scene s){
-		if(!mesh) return;
-		auto obj=s.createEntity3D();
-		obj.drawable = mesh;
-		obj.position = Vector3f(0, 0, 0);
-		auto land="extracted/prsc/prsc.WAD!/prsc.LAND";
-		//auto land="extracted/strato_a/ST_A.WAD!/ST_A.LAND";
-		//auto land="extracted/james_a/JA_A.WAD!/JA_A.LAND";
-		auto mapts=loadMAPTs(land);
-		//auto dts=loadDTs(land);
-		texture=New!Texture(null);
-		//bump=New!Texture(null);
-		//texture.image=mapts;
-		texture.image=mapts;
-		//bump.image=dts;
-		texture.createFromImage(texture.image);
-		//bump.createFromImage(texture.image);
-		auto mat=s.createMaterial();
-		mat.diffuse=texture;
-		//mat.height=bump;
-		//mat.parallax=ParallaxSimple;
-		mat.specular=texture;
-		mat.roughness=0;
-		mat.metallic=0;
-		obj.material=mat;
+		foreach(i,mesh;meshes){
+			if(!mesh) continue;
+			auto obj=s.createEntity3D();
+			obj.drawable = mesh;
+			obj.position = Vector3f(0, 0, 0);
+			//auto land="extracted/strato_a/ST_A.WAD!/ST_A.LAND";
+			//auto land="extracted/james_a/JA_A.WAD!/JA_A.LAND";
+			//auto dts=loadDTs(land);
+			//bump=New!Texture(null);
+			//texture.image=mapts;
+			//bump.image=dts;
+			//bump.createFromImage(texture.image);
+			auto mat=s.createMaterial();
+			assert(!!textures[i]);
+			mat.diffuse=textures[i];
+			//mat.diffuse=bump[dti[i]];
+			assert(!!bump[dti[i]]);
+			//mat.height=bump[dti[i]];
+			//mat.parallax=ParallaxSimple;
+			mat.specular=0;
+			mat.roughness=0;
+			mat.metallic=0;
+			obj.material=mat;
+		}
 	}
 }
 
-SuperImage loadDTs(string directory){
-	auto images=iota(0,7).map!(i=>loadTXTR(buildPath(directory,format("DT%02d.TXTR",i)))).array;
-	auto dti=loadDTIndex(directory).dts;
-	auto img=image(256*16*3,256*16*3);
-	foreach(i;0..256){
-		foreach(k;0..3){
-			foreach(l;0..3){
-				foreach(x;0..256){
-					foreach(y;0..256){
-						int cy=l==0?0:l==1?y:255;
-						int cx=k==0?0:k==1?x:255;
-						img[k*256+3*256*(i%16)+x,l*256+3*256*(i/16)+y]=images[dti[i]][cx,cy];
-					}
-				}
+SuperImage[] loadDTs(string directory){
+	auto r=iota(0,7).map!(i=>loadTXTR(buildPath(directory,format("DT%02d.TXTR",i)))).array;
+	foreach(ref img;r){
+		foreach(j;0..256){
+			foreach(i;0..256){
+				img[j,i]=Color4f(img[j,i].r,img[j,i].g,img[j,i].b,img[j,i].b);
 			}
 		}
 	}
-	//img.savePNG("test2.png");
-	return img;
+	return r;
 }
 
-SuperImage loadMAPTs(string directory){
-	auto img=image(64*16*3,64*16*3);
+SuperImage[] loadMAPTs(string directory){
 	auto palFile=buildPath(directory, "LAND.PALT");
 	auto palt=readFile(palFile);
 	palt=palt[8..$]; // header bytes (TODO: figure out what they mean)
-	foreach(i;0..256){
-		//auto maptFile=buildPath(directory,format("%04d.MAPT",i));
-		auto maptFile=buildPath(directory,format("%04d.MAPT",i));
-		if(!exists(maptFile)) continue;
-		auto data=readFile(maptFile);
-		import std.random;
-		/+int sum=0;
-		foreach(y;0..64){
-			foreach(x;0..64){
-				auto ccol=data[64*y+x];
-				sum+=palt[3*ccol];
-				sum+=palt[3*ccol+1];
-				sum+=palt[3*ccol+2];
-			}
-		}
-		sum/=64;+/
-		//auto color=Color4f(Color4(uniform(0,256),uniform(0,256),uniform(0,256)))*0.8;
-		/+Color4f color;
-		if(sum<44000) color=Color4f(0,0,0);
-		else color=Color4f(1,1,1);+/
-		
-		foreach(l;0..3){
-			foreach(k;0..3){
-				foreach(y;0..64){
-					foreach(x;0..64){
-						uint ccol;
-						int cy=l==0?0:l==1?y:63;
-						int cx=k==0?0:k==1?x:63;
-						ccol=data[64*cy+cx];
-						img[k*64+3*64*(i%16)+x,l*64+3*64*(i/16)+y]=Color4f(Color4(palt[3*ccol],palt[3*ccol+1],palt[3*ccol+2]))*0.6;
-					}
+	return iota(0,256).map!((i){
+			//auto maptFile=buildPath(directory,format("%04d.MAPT",i));
+			auto maptFile=buildPath(directory,format("%04d.MAPT",i));
+			auto img=image(64,64);
+			if(!exists(maptFile)) return img;
+			auto data=readFile(maptFile);
+			foreach(y;0..64){
+				foreach(x;0..64){
+					uint ccol=data[64*y+x];
+					img[x,y]=Color4f(Color4(palt[3*ccol],palt[3*ccol+1],palt[3*ccol+2]))*0.6;
 				}
 			}
-		}
-	}
-	//img.savePNG("test.png");
-	return img;
+			return img;
+		}).array;
 }
 
-Mesh createMesh(HMap hmap, TMap tmap, float scaleFactor=1){
+Mesh[] createMeshes(HMap hmap, TMap tmap, float scaleFactor=1){
 	auto edges=hmap.edges;
 	auto heights=hmap.heights.dup;
 	auto tiles=tmap.tiles;
@@ -117,79 +115,104 @@ Mesh createMesh(HMap hmap, TMap tmap, float scaleFactor=1){
 	foreach(h;heights) foreach(x;h) minHeight=min(minHeight,x);
 	foreach(h;heights) foreach(ref x;h) x-=minHeight;
 	//foreach(e;edges) e[]=false;
-	auto mesh=new Mesh(null);
+	Vector3f getVertex(int z,int x){
+		return scaleFactor*Vector3f(10*x,heights[z][x]/100,10*z);
+	}
 	auto n=to!int(hmap.edges.length);
 	enforce(n);
 	auto m=to!int(hmap.edges[0].length);
 	enforce(heights.length==n);
 	enforce(edges.all!(x=>x.length==m));
 	enforce(heights.all!(x=>x.length==m));
-	auto numVertices=4*(m-1)*(n-1);
-	mesh.vertices=New!(Vector3f[])(numVertices);
-	mesh.texcoords=New!(Vector2f[])(numVertices);
+	int di(int i){ return i==1||i==2; }
+	int dj(int i){ return i==2||i==3; }
+	auto getFaces(O)(int j,int i,O o){
+		if(!edges[j][i]&&!edges[j+1][i+1]&&!edges[j][i+1]) o.put([0,2,1]);
+		if(!edges[j+1][i+1]&&!edges[j][i]&&!edges[j+1][i]) o.put([2,0,3]);
+		if(edges[j][i]&&!edges[j][i+1]&&!edges[j+1][i+1]&&!edges[j+1][i]) o.put([1,3,2]);
+		if(edges[j+1][i+1]&&!edges[j][i]&&!edges[j][i+1]&&!edges[j+1][i]) o.put([0,3,1]);
+	}
+	auto normals=new Vector3f[][](n,m);
+	foreach(j;0..n) normals[j][]=Vector3f(0,0,0);
 	foreach(j;0..n-1){
 		foreach(i;0..m-1){
+			struct ProcessFaces{
+				void put(uint[3] f){
+					Vector3f[3] v;
+					foreach(k;0..3){
+						v[k]=getVertex(j+dj(f[k]),i+di(f[k]));
+					}
+					Vector3f p=cross(v[1]-v[0],v[2]-v[0]);
+					foreach(k;0..3){
+						normals[j+dj(f[k])][i+di(f[k])]+=p;
+					}
+				}
+			}
+			getFaces(j,i,ProcessFaces());
+		}
+	}
+	foreach(j;0..n)
+		foreach(i;0..m)
+			normals[j][i]=normals[j][i].normalized;
+	auto numVertices=new uint[](256);
+	auto numFaces=new uint[](256);
+	foreach(j;0..n-1){
+		foreach(i;0..m-1){
+			auto t=tiles[n-1-j][i];
+			int faces=0;
+			struct FaceCounter{
+				void put(uint[3]){
+					faces++;
+				}
+			}
+			getFaces(j,i,FaceCounter());
+			if(faces){
+				numVertices[t]+=4;
+				numFaces[t]+=faces;
+			}
+		}
+	}
+	auto curVertex=new uint[](256);
+	auto curFace=new uint[](256);
+	auto meshes=new Mesh[](256);
+	foreach(j;0..n-1){
+		foreach(i;0..m-1){
+			auto t=tiles[n-1-j][i];
+			if(!meshes[t]){
+				if(!numFaces[t]) continue;
+				meshes[t]=new Mesh(null);
+				meshes[t].vertices=New!(Vector3f[])(numVertices[t]);
+				meshes[t].normals=New!(Vector3f[])(numVertices[t]);
+				meshes[t].texcoords=New!(Vector2f[])(numVertices[t]);
+				meshes[t].indices=New!(uint[3][])(numFaces[t]);
+			}
+			int faces=0;
+			struct FaceCounter2{
+				void put(uint[3]){
+					faces++;
+				}
+			}
+			getFaces(j,i,FaceCounter2());
+			if(!faces) continue;
 			foreach(k;0..4){
-				int di=(k==1)+(k==2),dj=(k==2)+(k==3);
-				int x=i+di,z=j+dj;
-				mesh.vertices[4*((n-1)*i+j)+k]=scaleFactor*Vector3f(10*x,heights[z][x]/100,10*z);
-				enum eps=1/3.0f;
-				auto tile=tiles[n-1-j][i];
-				mesh.texcoords[4*((n-1)*i+j)+k]=Vector2f((tile%16+(di?1-eps:eps))/16.0f,(tile/16+(!dj?1-eps:eps))/16.0f);
+				meshes[t].vertices[curVertex[t]+k]=getVertex(j+dj(k),i+di(k));
+				meshes[t].normals[curVertex[t]+k]=normals[j+dj(k)][i+di(k)];
+				meshes[t].texcoords[curVertex[t]+k]=Vector2f(di(k),!dj(k));
 			}
-		}
-	}
-	mesh.indices=New!(uint[3][])(2*(m-1)*(n-1));
-	foreach(int j;0..n-1){
-		foreach(int i;0..m-1){
-			if(!edges[j][i]&&!edges[j+1][i+1]&&!edges[j][i+1])
-				mesh.indices[2*((n-1)*i+j)]=[4*((n-1)*i+j)+0,4*((n-1)*i+j)+2,4*((m-1)*i+j)+1];
-			if(!edges[j+1][i+1]&&!edges[j][i]&&!edges[j+1][i])
-				mesh.indices[2*((n-1)*i+j)+1]=[4*((n-1)*i+j)+2,4*((n-1)*i+j)+0,4*((m-1)*i+j)+3];
-			if(edges[j][i]&&!edges[j][i+1]&&!edges[j+1][i+1]&&!edges[j+1][i])
-				mesh.indices[2*((n-1)*i+j)]=[4*((n-1)*i+j)+1,4*((n-1)*i+j)+3,4*((m-1)*i+j)+2];
-			if(edges[j+1][i+1]&&!edges[j][i]&&!edges[j][i+1]&&!edges[j+1][i])
-				mesh.indices[2*((n-1)*i+j)+1]=[4*((n-1)*i+j)+0,4*((n-1)*i+j)+3,4*((m-1)*i+j)+1];
-		}
-	}
-	mesh.normals=New!(Vector3f[])(numVertices);
-	mesh.normals[] = Vector3f(0.0f, 0.0f, 0.0f);
-	foreach(ref f;mesh.indices){
-		auto v0=mesh.vertices[f[0]], v1=mesh.vertices[f[1]], v2=mesh.vertices[f[2]];
-		Vector3f p=cross(v1-v0,v2-v0);
-		auto clones(int x){
-			int offset=x&3;
-			int location=x/4;
-			int i=location/(n-1);
-			int j=location%(n-1);
-			assert(0<=i&&i<m-1);
-			assert(0<=j&&j<n-1);
-			typeof(only(n,n,n)) r;
-			final switch(offset){
-				case 0:
-					r=only(4*((n-1)*(i-1)+j)+1,4*((n-1)*i+(j-1))+3,4*((n-1)*(i-1)+(j-1))+2);
-					break;
-				case 1:
-					r=only(4*((n-1)*i+(j-1))+2,4*((n-1)*(i+1)+(j-1))+3,4*((n-1)*(i+1)+j));
-					break;
-				case 2:
-					r=only(4*((n-1)*(i+1)+j)+3,4*((n-1)*i+(j+1))+1,4*((n-1)*(i+1)+(j+1)));
-					break;
-				case 3:
-					r=only(4*((n-1)*(i-1)+j)+2,4*((n-1)*(i-1)+(j+1))+1,4*((n-1)*i+(j+1)));
-				break;
+			struct ProcessFaces2{
+				void put(uint[3] f){
+					meshes[t].indices[curFace[t]++]=[curVertex[t]+f[0],curVertex[t]+f[1],curVertex[t]+f[2]];
+				}
 			}
-			return chain(only(x),r.filter!(x=>0<=x&&x<mesh.normals.length));
-		}
-		foreach(i;0..3){
-			//foreach(v;clones(f[i])) assert(mesh.vertices[v]==mesh.vertices[f[i]],text(v," ",f[i]));
-			foreach(v;clones(f[i])) mesh.normals[v]+=p;
+			getFaces(j,i,ProcessFaces2());
+			curVertex[t]+=4;
 		}
 	}
-	foreach(i,normal;mesh.normals){
-		mesh.normals[i] = normal.normalized;
+	assert(curVertex==numVertices && curFace==numFaces);
+	foreach(mesh;meshes){
+		if(!mesh) continue;
+		mesh.dataReady=true;
+		mesh.prepareVAO();
 	}
-	mesh.dataReady=true;
-	mesh.prepareVAO();
-	return mesh;
+	return meshes;
 }
