dc983ca456aae80e29d1e86262f6b2e355ecacab
tg <tg@example.com>
2018-12-21 20:54:51 +0100

    Disable SSAO by default.

--- source/options.d
+++ source/options.d
@@ -4,7 +4,7 @@ struct Options{
 	bool enableWidgets=true;
 	bool enableMapBottom=true;
 	bool enableFog=false;
-	bool enableSSAO=true;
+	bool enableSSAO=false;
 	bool enableGlow=true;
 	float glowBrightness=0.5;
 	bool enableAntialiasing=true;
