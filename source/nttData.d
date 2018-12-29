5f2c04ac4469fe235f27be37014d713be019bc5a
tg <tg@example.com>
2018-12-29 04:34:23 +0100

    Remove mannual creature scaling data.

--- source/nttData.d
+++ source/nttData.d
@@ -225,7 +225,6 @@ immutable struct CreatureData{
 	string name; // TODO: internationalization
 	string model;
 	string stance;
-	float scaling=2e-3;
 	float zfactorOverride=float.nan;
 	auto rotateOnGround=RotateOnGround.no;
 }
@@ -233,334 +232,282 @@ immutable struct CreatureData{
 CreatureData abomination={
 	tag: "ctug",
 	name: "Abomination",
-	scaling: 2e-3,
 };
 
 CreatureData astaroth={
 	tag: "RAMH",
 	name: "Astaroth",
-	scaling: 2e-3,
 };
 
 CreatureData basilisk={
 	tag: "guls",
 	name: "Basilisk",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData blight={
 	tag: "kacd",
 	name: "Blight",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData bombard={
 	tag: "wlcf",
 	name: "Bombard",
-	scaling: 2e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData boulderdash={
 	tag: "llab",
 	name: "Boulderdash",
-	scaling: 2e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData brainiac={
 	tag: "bobs",
 	name: "Brainiac",
-	scaling: 1e-3,
 };
 
 CreatureData cog={
 	tag: "zidf",
 	name: "Cog",
-	scaling: 1e-3,
 };
 
 CreatureData deadeye={
 	tag: "plfd",
 	name: "Deadeye",
-	scaling: 1e-3,
 };
 
 CreatureData dragon={
 	tag: "grdg",
 	name: "Dragon",
-	scaling: 4e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData dragonHatchling={
 	tag: "rdbO",
 	name: "Dragon Hatchling",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData druid={
 	tag: "nmuh",
 	name: "Druid",
-	scaling: 1e-3,
 };
 
 CreatureData earthfling={
 	tag: "palk",
 	name: "Earthfling",
-	scaling: 1e-3,
 };
 
 CreatureData ent={
 	tag: "mtsl",
 	name: "Ent",
-	scaling: 2e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData faestus1={
 	tag: "ehtH",
 	name: "Faestus",
-	scaling: 1.5e-3,
 };
 
 CreatureData faestus2={
 	tag: "EHTH",
 	name: "Faestus",
-	scaling: 1.5e-3,
 };
 
 CreatureData fallen={
 	tag: "dplk",
 	name: "Fallen",
-	scaling: 1e-3,
 };
 
 CreatureData familiar={
 	tag: "imaf",
 	name: "Familiar",
-	scaling: 1e-3,
 };
 
 CreatureData farmer={
 	tag: "zepe",
 	name: "Farmer",
-	scaling: 1e-3,
 };
 
 CreatureData firefist={
 	tag: "lrtf",
 	name: "Firefist",
-	scaling: 2e-3,
 };
 
 CreatureData flameminion={
 	tag: "fplk",
 	name: "Earthfling",
-	scaling: 1e-3,
 };
 
 CreatureData flummox={
 	tag: "wlce",
 	name: "Flummox",
-	scaling: 1.5e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData flurry={
 	tag: "wlca",
 	name: "Flurry",
-	scaling: 1.5e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData frostwolf={ // TODO: this is screwed up, why?
 	tag: "lbog",
 	name: "Frostwolf",
-	scaling: 1e-3,
 	zfactorOverride: 1.0f,
 };
 
 CreatureData gammel={
 	tag: "magH",
 	name: "Gammel",
-	scaling: 1.5e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData gangrel={
 	tag: "ramH",
 	name: "Gangrel",
-	scaling: 2e-3,
 };
 
 CreatureData gargoyle={
 	tag: "sohe",
 	name: "Gargoyle",
-	scaling: 1e-3,
 };
 
 CreatureData ghost={
 	tag: "tshg",
 	name: "Ghost",
-	scaling: 1e-3,
 };
 
 CreatureData gnome={
 	tag: "plfl",
 	name: "Gnome",
-	scaling: 1e-3,
 };
 
 CreatureData gremlin={
 	tag: "lrps",
 	name: "Gremlin",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData hellmouth={
 	tag: "nomd",
 	name: "Hellmouth",
-	scaling: 3e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData ikarus={
 	tag: "kace",
 	name: "Ikarus",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData jabberocky={
 	tag: "mtse",
 	name: "Jabberocky",
-	scaling: 2e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData locust={
 	tag: "pazb",
 	name: "Locust",
-	scaling: 1e-3,
 };
 
 CreatureData lordSurtur={
 	tag: "uslH",
 	name: "Lord Surtur",
-	scaling: 2.5e-3,
 };
 
 CreatureData manahoar={
 	tag: "oham",
 	name: "Manahoar",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData mutant={
 	tag: "cbab",
 	name: "Mutant",
-	scaling: 2e-3,
 };
 
 CreatureData necryl={
 	tag: "glsd",
 	name: "Necryl",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData netherfiend={
 	tag: "crpd",
 	name: "Netherfiend",
-	scaling: 2e-3,
 };
 
 CreatureData peasant={
 	tag: "saep",
 	name: "Peasant",
-	scaling: 1e-3,
 };
 
 CreatureData phoenix={
 	tag: "grdr",
 	name: "Phoenix",
-	scaling: 4e-3,
 };
 
 CreatureData pyrodactyl={
 	tag: "kacf",
 	name: "Pyrodactyl",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData pyromaniac={
 	tag: "plff",
 	name: "Pyromaniac",
-	scaling: 1e-3,
 };
 
 CreatureData ranger={
 	tag: "amuh",
 	name: "Ranger",
-	scaling: 1e-3,
 };
 
 CreatureData rhinok={
 	tag: "gard",
 	name: "Rhinok",
-	scaling: 3e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData sacDoctor={
 	tag: "dcas",
 	name: "Sac Doctor",
-	scaling: 2e-3,
 };
 
 CreatureData saraBella={
 	tag: "pezH",
 	name: "Sara Bella",
-	scaling: 2e-3,
 };
 
 CreatureData scarab={
 	tag: "cara",
 	name: "Scarab",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData scythe={
 	tag: "dzid",
 	name: "Scythe",
-	scaling: 1e-3,
 };
 
 CreatureData seraph={
 	tag: "grps",
 	name: "Seraph",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData shrike={
 	tag: "tbsh",
 	name: "Shrike",
-	scaling: 1e-3,
 };
 
 CreatureData silverback={
 	tag: "grdb",
 	name: "Silverback",
-	scaling: 3e-3,
 	zfactorOverride: 1.0,
 	rotateOnGround: RotateOnGround.completely,
 };
@@ -568,57 +515,48 @@ CreatureData silverback={
 CreatureData sirocco={
 	tag: "risH",
 	name: "Sirocco",
-	scaling: 5e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData slave={
 	tag: "zepf",
 	name: "Slave",
-	scaling: 1e-3,
 };
 
 CreatureData snowman={
 	tag: "zepa",
 	name: "Snowman",
-	scaling: 1e-3,
 };
 
 CreatureData spitfire={
 	tag: "sohf",
 	name: "Spitfire",
-	scaling: 1e-3,
 };
 
 CreatureData squall={
 	tag: "alab",
 	name: "Squall",
-	scaling: 1.5e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData stormGiant={
 	tag: "rgos",
 	name: "Storm Giant",
-	scaling: 2e-3,
 };
 
 CreatureData styx={
 	tag: "nugd",
 	name: "Styx",
-	scaling: 2e-3,
 };
 
 CreatureData sylph={
 	tag: "ahcr",
 	name: "Sylph",
-	scaling: 1e-3,
 };
 
 CreatureData taurock={
 	tag: "raeb",
 	name: "Taurock",
-	scaling: 2e-3,
 	zfactorOverride: 0.8,
 	rotateOnGround: RotateOnGround.completely,
 };
@@ -626,160 +564,135 @@ CreatureData taurock={
 CreatureData thestor={
 	tag: "eafH",
 	name: "Thestor",
-	scaling: 1.5e-3,
 };
 
 CreatureData tickferno={
 	tag: "craf",
 	name: "Tickferno",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData toldor={
 	tag: "oohH",
 	name: "Toldor",
-	scaling: 2.5e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData trogg={
 	tag: "ycro",
 	name: "Trogg",
-	scaling: 1e-3,
 };
 
 CreatureData troll={
 	tag: "lort",
 	name: "Troll",
-	scaling: 2e-3,
 };
 
 CreatureData vortick={
 	tag: "craa",
 	name: "Vortick",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData warmonger={
 	tag: "nugf",
 	name: "Warmonger",
-	scaling: 2e-3,
 };
 
 CreatureData yeti={
 	tag: "ycrp",
 	name: "Yeti",
-	scaling: 2e-3,
 };
 
 CreatureData zombie={
 	tag: "zepd",
 	name: "Zombie",
-	scaling: 1e-3,
 };
 
 CreatureData zyzyx={
 	tag: "tnem",
 	name: "Zyzyx",
-	scaling: 1e-3,
 };
 
 CreatureData abraxus={
 	tag: "0ewc",
 	name: "Abraxus",
-	scaling: 1e-3,
 };
 
 CreatureData acheron={
 	tag: "1dwc",
 	name: "Acheron",
-	scaling: 1e-3,
 };
 
 CreatureData ambassadorButa={
 	tag: "0fwc",
 	name: "Ambassador Buta",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData charlotte={
 	tag: "2fwc",
 	name: "Charlotte",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData eldred={
 	tag: "2ewc",
 	name: "Eldred",
-	scaling: 1e-3,
 };
 
 CreatureData grakkus={
 	tag: "1fwc",
 	name: "Grakkus",
-	scaling: 1e-3,
 };
 
 CreatureData hachimen={
 	tag: "2lwc",
 	name: "Hachimen",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.sideways,
 };
 
 CreatureData jadugarr={
 	tag: "0awc",
 	name: "Jadugarr",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData marduk={
 	tag: "2awc",
 	name: "Marduk",
-	scaling: 1e-3
 };
 
 CreatureData mithras={
 	tag: "1ewc",
 	name: "Mithras",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData seerix={
 	tag: "1awc",
 	name: "Seerix",
-	scaling: 1e-3,
 	rotateOnGround: RotateOnGround.completely,
 };
 
 CreatureData shakti={
 	tag: "0lwc",
 	name: "Shakti",
-	scaling: 1e-3
 };
 
 CreatureData sorcha={
 	tag: "2dwc",
 	name: "Sorcha",
-	scaling: 1e-3,
 };
 
 CreatureData theRagman={
 	tag: "0dwc",
 	name: "The Ragman",
-	scaling: 1e-3,
 };
 
 CreatureData yogo={
 	tag: "1lwc",
 	name: "Yogo",
-	scaling: 1e-3
 };
 
 CreatureData* creatureDataByTag(char[4] tag){
