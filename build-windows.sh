eb6dbb316e899417b55ab72c3663d4402fcce796
tg <tg@example.com>
2019-07-30 01:18:06 +0200

    Add SacEngineDebug zips.

--- build-windows.sh
+++ build-windows.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # TODO: figure out how to pass only -O -inline
+d=$(date +%Y-%m-%d-%H-%M-%S)
 wine ./ldc2-1.16.0-windows-x64/bin/dub.exe build --compiler=./ldc2-1.16.0-windows-x64/bin/ldc2.exe
+cp 3d.exe SacEngineDebug-$d.exe
+strip SacEngineDebug-$d.exe
+zip SacEngineDebug-$d.zip SacEngineDebug-$d.exe SDL2.dll freetype.dll libmpg123-0.dll
