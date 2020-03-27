b4efbfff2e8f1e2c92bbf319599dcd018a605911
tg <tg@example.com>
2020-03-27 03:42:14 +0100

    Basis for hotkeys.

---
+++ source/hotkeys.d
@@ -0,0 +1,105 @@
+
+enum Bindable:char[4]{
+	// control keys
+	moveForward="rofM",
+	moveBackward="kabM",
+	turnLeft="tflM",
+	turnRight="tgrM",
+	cameraZoomIn="imzM",
+	cameraZoomOut="omzM",
+	// orders
+	attack="ttat",
+	guard="augt",
+	retreat="ziwp",
+	move="otog",
+	useAbility="ibaK",
+	dropSoul="losd",
+	// miscellaneous
+	optionsMenu="UNEM",
+	skipSpeech="piks",
+	nextSpellTab="nbat", // "nepo"
+	creationSpells="8rcS", // "nepo"
+	structureSpells="rtsS", // "nepo"
+	quickSave="vasQ",
+	quickLoad="dolQ",
+	pause="swap",
+	changeCamera="macc",
+	sendChatMessage="thcK",
+	gammaCorrectionPlus="+mag", // "amag"		
+	gammaCorrectionMinus="-mag", // "amag"
+	screenShot="tohs",
+	// formations
+	semicircleFormation="mesf",
+	circleFormation="ricf",
+	phalanxFormation="ahpf",
+	wedgeFormation="jdwf",
+	skirmishFormation="rksf",
+	lineFormation="nilf",
+	flankLeftFormation="llff",
+	flankRightFormation="rlff",
+	// taunts
+	randomTaunt="tntr",
+	taunt1="1ntK", // "tntK"
+	taunt2="2ntK", // "tntK"
+	taunt3="3ntK", // "tntK"
+	taunt4="4ntK", // "tntK"
+	// cast creation spell #
+	castCreationSpell1="1rcK", // "lpsK "?rcK" 1
+	castCreationSpell2="2rcK", // "lpsK "?rcK" 2
+	castCreationSpell3="3rcK", // "lpsK "?rcK" 3
+	castCreationSpell4="4rcK", // "lpsK "?rcK" 4
+	castCreationSpell5="5rcK", // "lpsK "?rcK" 5
+	castCreationSpell6="6rcK", // "lpsK "?rcK" 6
+	castCreationSpell7="7rcK", // "lpsK "?rcK" 7
+	castCreationSpell8="8rcK", // "lpsK "?rcK" 8
+	castCreationSpell9="9rcK", // "lpsK "?rcK" 9
+	// cast spell #
+	castSpell1="1psK", // "lpsK" "?psK" 1
+	castSpell2="2psK", // "lpsK" "?psK" 2
+	castSpell3="3psK", // "lpsK" "?psK" 3
+	castSpell4="4psK", // "lpsK" "?psK" 4
+	castSpell5="5psK", // "lpsK" "?psK" 5
+	castSpell6="6psK", // "lpsK" "?psK" 6
+	castSpell7="7psK", // "lpsK" "?psK" 7
+	castSpell8="8psK", // "lpsK" "?psK" 8
+	castSpell9="9psK", // "lpsK" "?psK" 9
+	castSpell10="01sK", // "lpsK" "?psK" 10
+	castSpell11="11sK", // "lpsK" "?psK" 11
+	// cast structure spell #
+	castStructureSpell1="1hsK", // "lpsK" "?hsK" 1
+	castStructureSpell2="2hsK," // "lpsK" "?hsK" 2
+	castStructureSpell3="2hsK," // "lpsK" "?hsK" 3
+	castStructureSpell4="2hsK," // "lpsK" "?hsK" 4
+	castStructureSpell5="2hsK," // "lpsK" "?hsK" 5	
+	castStructureSpell6="2hsK," // "lpsK" "?hsK" 6
+	// cast neutral spell
+	castManalith="htlm", // "lpsK"
+	castManahoar="oham", // "lpsK"
+	castSpeedUp="pups", // "lpsK"
+	castGuardian="ndrg", // "lpsK"
+	castConvert="ccas", // "lpsK"
+	castDesecrate="ucas", // "lpsK"
+	castTeleport="elet", // "lpsK"
+	castHeal="laeh", // "lpsK",
+	castShrine="pcas", // "lpsK"
+	// cast persephone spell
+	// TODO
+	// cast persephone faithful
+	// TODO
+	// cast pyro spell
+	// TODO
+	// cast pyro's Prole
+	// TODO
+	// cast james spell
+	// TODO
+	// cast james' men o' the glebe
+	// TODO
+	// cast stratos spell
+	// TODO
+	// cast stratos servants
+	// TODO
+	// cast charnel spell
+	// TODO
+	// cast charnel's minions
+	// TODO
+}
