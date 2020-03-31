c77c217d93656fb51e59a80636bf4aea62f819b1
tg <tg@example.com>
2020-03-31 05:04:03 +0200

    Package hotkeys.txt with windows release archivews.

--- build-windows-release.sh
+++ build-windows-release.sh
@@ -4,4 +4,4 @@ d=$(date +%Y-%m-%d-%H-%M-%S)
 wine ./ldc2-1.16.0-windows-x64/bin/dub.exe build -b release-debug --compiler=./ldc2-1.16.0-windows-x64/bin/ldc2.exe
 cp 3d.exe SacEngine-$d.exe
 strip SacEngine-$d.exe
-zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt
+zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
--- build-windows.sh
+++ build-windows.sh
@@ -2,6 +2,6 @@
 # TODO: figure out how to pass only -O -inline
 d=$(date +%Y-%m-%d-%H-%M-%S)
 wine ./ldc2-1.16.0-windows-x64/bin/dub.exe build --compiler=./ldc2-1.16.0-windows-x64/bin/ldc2.exe
-cp 3d.exe SacEngineDebug-$d.exe
-strip SacEngineDebug-$d.exe
-zip SacEngineDebug-$d.zip SacEngineDebug-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt
+# cp 3d.exe SacEngineDebug-$d.exe
+# strip SacEngineDebug-$d.exe
+# zip SacEngineDebug-$d.zip SacEngineDebug-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
