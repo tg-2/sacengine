6f24915b8016ca81665c96a6d8b86fa189f6c5a3
tg <tg@example.com>
2021-11-11 12:59:44 +0100

    Fix Erupt displacement.

--- source/state.d
+++ source/state.d
@@ -2702,7 +2702,7 @@ struct Erupt(B){
 			float wavePos=abs(dist-waveLoc)/waveSize;
 			float waveHeight=height*(1.0f-progress);
 			if(wavePos<1.0f) displacement+=0.5f*0.4f*(1.0f+cos(pi*wavePos))*waveHeight;
-			displacement-=(1.0f+cos(pi*dist/waveRange))*sin(pi*progress)*reboundHeight;
+			if(dist<waveRange) displacement-=(1.0f+cos(pi*dist/waveRange))*sin(pi*progress)*reboundHeight;
 		}
 		return displacement;
 		/*auto shape=0.6f*(1.0f-dist/range);
