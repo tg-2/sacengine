41bc1debf26299260aa7d1e3a5ac4e3c3a77b7c4
tg <tg@example.com>
2021-12-29 14:36:16 +0100

    Update .gitignore and host script.

--- host.sh
+++ host.sh
@@ -1,6 +1,6 @@
-event=1v1-horse
+event=1v1-horse-test
 
-# map='maps/(2) Ferry.scp'
+map='maps/(2) Ferry.scp'
 # map='maps/(2) WM-ColdKiss.scp'
 # map='maps/(2) TM-Chain of Being.scp'
 # map='maps/(2) TM-Glaciers.scp'
@@ -12,17 +12,18 @@ event=1v1-horse
 #map='maps/(4) Ferry.scp'
 #map='maps/(4) Ferry.scp'
 
-map=--map-list=maps-1v1.txt
+#map=--map-list=maps-1v1.txt
 #map=--map-list=maps-2v2.txt
+#map=--map-list=maps-3ffa.txt
 echo $map
 #map='maps/(2) Oval kopia.scp'
 
-level=1
+level=4
 souls=12
 wizard=sorcha
 NAME=$(date +%Y-%m-%d-%H-%M-%S)
 echo "run" > /tmp/cmds
-#gdb -x /tmp/cmds --args ./SacEngine-current --resolution=1080 --cursor-size=64 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --random-spellbooks --shuffle-sides --record=replays/"$event"-"$NAME".scr --host
-./build-release.sh && gdb -x /tmp/cmds --args ./3d --resolution=1080 --cursor-size=64 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --random-spellbooks --shuffle-sides --record=replays/"$event"-"$NAME".rcp --host
-# ./build-ldc.sh && gdb -x /tmp/cmds --args ./3d --resolution=2160 --shadow-map-resolution=10000 --cursor-size=128 --level=$level --souls=$souls --wizard=sorcha --record=replays/"$event"-"$NAME".scr --souls=$souls "$map" --no-widgets --random-spellbooks --shuffle-sides --host
-
+#gdb -x /tmp/cmds --args ./SacEngine-current --host --resolution=1080 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --record=replays/"$event"-"$NAME".rcp --random-spellbooks --shuffle-sides
+./build-release.sh && gdb -x /tmp/cmds --args ./3d --host --resolution=1080 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --record=replays/"$event"-"$NAME".rcp --random-spellbooks --shuffle-sides
+# ./build-ldc.sh && gdb -x /tmp/cmds --args ./3d --host --resolution=1080 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --record=replays/"$event"-"$NAME".rcp --random-spellbooks --shuffle-sides
+#wine explorer /desktop=1920x1080 SacEngine-2021-12-26-20-56-33.exe --host --resolution=1080 --level=$level --souls=$souls --wizard=sorcha --souls=$souls "$map" --record=replays/"$event"-"$NAME".rcp --random-spellbooks --shuffle-sides
