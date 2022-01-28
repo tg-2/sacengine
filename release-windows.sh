bca0a0030b30bb60dd1b1215a8d8d5415d8725db
tg <tg@example.com>
2022-01-28 13:15:58 +0100

    Ship OpenAL dlls with windows releases.

--- release-windows.sh
+++ release-windows.sh
@@ -3,4 +3,4 @@ d=$(date +%Y-%m-%d-%H-%M-%S)
 ./build-windows-release.sh
 cp 3d.exe SacEngine-$d.exe
 strip SacEngine-$d.exe
-zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
+zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll OpenAL32.dll wrap_oal.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
