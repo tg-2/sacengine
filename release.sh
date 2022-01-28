fad8114f8917eb64220c359f0088f301a4dee495
tg <tg@example.com>
2022-01-28 15:15:14 +0100

    Actually ship OpenAL dlls in windows release.

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ d=$(date +%Y-%m-%d-%H-%M-%S)
 ./build-windows-release.sh
 cp 3d.exe SacEngine-$d.exe
 strip SacEngine-$d.exe
-zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
+zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll OpenAL32.dll wrap_oal.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
 
 ./build-release.sh
 cp 3d SacEngine-current
