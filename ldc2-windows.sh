e3e1a4a9038d27a3fea3f2b47b32caf0879e5573
tg <tg@example.com>
2022-01-29 16:50:13 +0100

    Fix typo.

--- ldc2-windows.sh
+++ ldc2-windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./ldc2-1.28.0-linux-x86_64/bin/ldc2 -mtriple=x86_64-windows-msvc $@
 
-# in ldc2-1.28.0-windows-x64/etc/ldc2.conf:
+# in ldc2-1.28.0-linux-x86_64/etc/ldc2.conf:
 
 # "x86_64-.*-windows-msvc":
 # {
