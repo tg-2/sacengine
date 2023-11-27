85ee3f94af81cbe7d2f9a22916aa6d7f8ae4f5fe
tg <tg@example.com>
2023-11-27 04:07:48 +0100

    Re-enable stutter-free rejoin and late join.

--- source/controller.d
+++ source/controller.d
@@ -233,12 +233,10 @@ final class Controller(B){
 			}
 			if(network.hostDropped) return true;
 			if(network.lateJoining){
-				//pauseTimerOnPause=false; // stutter-free rejoin. TODO: this is only needed if game is not paused
-				pause();
-				// network.updateStatus(PlayerStatus.pendingLoad); // stutter-free rejoin
-				network.updateStatus(PlayerStatus.desynched);
-				//pauseTimerOnPause=false; // stutter-free rejoin. TODO: this is only needed if game is not paused
-				return true;
+				if(!network.paused&&!network.desynched){
+					// stutter-free late join
+					network.updateStatus(PlayerStatus.pendingLoad);
+				}else network.updateStatus(PlayerStatus.desynched);
 			}
 			if(network.lateJoining||network.players[network.me].status==PlayerStatus.pendingLoad){
 				pauseTimerOnPause=false;
