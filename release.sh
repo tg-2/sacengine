a50b9645117055ac4d7cf6850062ab3baaa41cae
tg <tg@example.com>
2023-11-30 18:30:29 +0100

    Add OS X release to script.

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 d=$(date +%Y-%m-%d-%H-%M-%S)
 echo $d
-./release-windows.sh $d & ./release-linux.sh $d
+./release-windows.sh $d & ./release-linux.sh $d & ./remote-release-osx.sh $d
 wait
