17ba175ad1396271ec63989d4896d465f4c5007a
tg <tg@example.com>
2023-11-30 18:23:27 +0100

    Fix.

--- release-osx.sh
+++ release-osx.sh
@@ -4,4 +4,4 @@ d=${1:-$(date +%Y-%m-%d-%H-%M-%S)}
 cp 3d SacEngine-current
 cp 3d SacEngine-$d
 strip SacEngine-$d
-zip SacEngine-$d.zip SacEngine-$d libSDL2.dylib libfreetype.6.dylib libzt.dylib settings.txt hotkeys.txt
+zip SacEngine-$d-osx.zip SacEngine-$d libSDL2.dylib libfreetype.6.dylib libzt.dylib settings.txt hotkeys.txt
