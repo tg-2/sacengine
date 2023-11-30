55e839a3ead7a261a04bf16542b380e43cfc56a7
tg <tg@example.com>
2023-11-30 18:54:04 +0100

    Fix.

--- remote-release-osx.sh
+++ remote-release-osx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 d=${1:-$(date +%Y-%m-%d-%H-%M-%S)}
-ssh tg@sacengine-osx-builder -C "eval \"\$(/opt/homebrew/bin/brew shellenv)\" && cd ~/Desktop/sacengine && ./release-osx.sh $d"
+ssh tg@sacengine-osx-builder -C "eval \"\$(/opt/homebrew/bin/brew shellenv)\" && cd ~/Desktop/sacengine && git pull --autostash && ./release-osx.sh $d"
 rsync -arzihv tg@sacengine-osx-builder:"~/Desktop/sacengine/SacEngine-$d-osx.zip" .
