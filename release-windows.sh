27b0549bb56e460226867a55ac4f742f2dbac8a2
tg <tg@example.com>
2023-11-30 17:56:23 +0100

    Add libzt to windows release script.

--- release-windows.sh
+++ release-windows.sh
@@ -4,4 +4,4 @@ d=${1:-$(date +%Y-%m-%d-%H-%M-%S)}
 cp 3d.exe SacEngine-current.exe
 cp 3d.exe SacEngine-$d.exe
 strip SacEngine-$d.exe
-zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll OpenAL32.dll wrap_oal.dll freetype.dll libmpg123-0.dll settings.txt hotkeys.txt
+zip SacEngine-$d.zip SacEngine-$d.exe SDL2.dll OpenAL32.dll wrap_oal.dll freetype.dll libmpg123-0.dll libzt.dll settings.txt hotkeys.txt
