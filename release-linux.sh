f4ff74f2408ad391277d9992373a0684f2c93b2a
tg <tg@example.com>
2023-12-01 05:09:30 +0100

    Fix.

--- release-linux.sh
+++ release-linux.sh
@@ -4,4 +4,4 @@ d=${1:-$(date +%Y-%m-%d-%H-%M-%S)}
 cp 3d SacEngine-current
 cp 3d SacEngine-$d
 strip SacEngine-$d
-zip SacEngine-$d-linux.zip SacEngine-$d
+zip SacEngine-$d-linux.zip libzt.so SacEngine-$d
