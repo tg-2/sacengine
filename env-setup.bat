669bdb76454ccf00f25f8be88982d3f0a46f2697
RkShaRkz <darksidemetatron@gmail.com>
2024-05-01 17:25:12 +0200

    Another windows build script for utilizing the lowmem flag
    Made a separate -lowmem build script for windows.
    Also changes to the env-setup script because the old one went in a wrong direction;
    due to lack of recursive clone, it tried to fix the dependencies which were necessary as submodules

--- env-setup.bat
+++ env-setup.bat
@@ -2,6 +2,8 @@ ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib
 ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib:math
 ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib:math:portable
 
-ldc2-1.35.0-windows-x64\bin\dub.exe add dlib
+ldc2-1.35.0-windows-x64\bin\dub.exe build
+
+REM ldc2-1.35.0-windows-x64\bin\dub.exe add dlib
 REM ldc2-1.35.0-windows-x64\bin\dub.exe add dlib:math
 REM ldc2-1.35.0-windows-x64\bin\dub.exe add dlib:math:portable
