faf58cd1d76db966331fd8d86cff5f26f2fd251b
RkShaRkz <darksidemetatron@gmail.com>
2024-05-01 16:22:40 +0200

    Two scripts for using on windows

---
+++ build-windows.bat
@@ -0,0 +1,1 @@
+ldc2-1.35.0-windows-x64\bin\dub.exe build -b release-debug --compiler=./ldc2-1.35.0-windows-x64/bin/ldc2.exe
---
+++ env-setup.bat
@@ -0,0 +1,7 @@
+ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib
+ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib:math
+ldc2-1.35.0-windows-x64\bin\dub.exe fetch dlib:math:portable
+
+ldc2-1.35.0-windows-x64\bin\dub.exe add dlib
+REM ldc2-1.35.0-windows-x64\bin\dub.exe add dlib:math
+REM ldc2-1.35.0-windows-x64\bin\dub.exe add dlib:math:portable
