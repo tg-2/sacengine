1e4325e91c1ac8117072a762f87832e8e8d04cf5
tg <tg@example.com>
2024-05-08 21:58:18 +0200

    Add code to compare to Tree's measured casting times.

--- source/testCastingTimes_.d
+++ source/testCastingTimes_.d
@@ -3,12 +3,14 @@
 // https://www.gnu.org/licenses/gpl-3.0.txt
 
 import state,sacobject,sacspell,ntts,nttData,animations;
-import std.traits, std.range, std.algorithm, std.stdio, std.typecons, std.array, std.conv;
+import std.traits, std.range, std.algorithm, std.stdio, std.typecons, std.array, std.string, std.conv;
 import dlib.math, dlib.math.portable;
 
 void testCastingTimes(B)(){
 	auto state=B.state.current;
 	Spellbook!B spellbook;
+	void[0][Tuple!(string,string,int,int)] valid; // wizard name, spell name, level, cycles
+	int[][Tuple!(string,string,int)] cycleMap; // wizard name, spall name, level -> cycles
 	foreach(wtag;[EnumMembers!WizardTag]){
 		auto obj=SacObject!B.getSAXS!Wizard(wtag);
 		writeln("Casting cycles for ",obj.name,":");
@@ -38,6 +40,8 @@ void testCastingTimes(B)(){
 						}
 					}
 					levelsForCycles[stationary][cycles]~=level;
+					valid[tuple(obj.name.toLower,spell.name.toLower,level,cycles)]=[];
+					cycleMap[tuple(obj.name.toLower,spell.name.toLower,level)]~=cycles;
 				}
 				auto kv=levelsForCycles[stationary].byKeyValue.map!(x=>tuple(x.key,x.value)).array.sort!"a[0]>b[0]".release;
 				foreach(cycles,level;kv.map!(x=>x)){
@@ -47,6 +51,28 @@ void testCastingTimes(B)(){
 		}
 	}
 	stdout.flush();
+	import std.file:readText;
+	auto treeText=readText("casting-times-tree.txt").splitLines;
+	auto indices=enumerate(treeText).filter!(x=>x[1].strip=="spell\tlevel\tcycles").map!(x=>x[0]-1).array;
+	foreach(i;1..indices.length){
+		auto wizard=treeText[indices[i-1]];
+		foreach(line;treeText[indices[i-1]+2..indices[i]-1]){
+			if(line.stripLeft.startsWith("#")) continue;
+			auto cells=line.split("\t");
+			auto spells=cells[0].split(",").map!(x=>x.strip).array;
+			auto levels=text("[",cells[1],"]").to!(int[]);
+			auto cycless=text("[",cells[2],"]").to!(int[]);
+			foreach(spell,level,cycles;cartesianProduct(spells,levels,cycless)){
+				if(tuple(wizard.toLower,spell.toLower,level,cycles) !in valid){
+					if(tuple(wizard.toLower,spell.toLower,level) in cycleMap){
+						writeln("wrong: need ",wizard,", ",spell,", lv",level,", ",cycles," cycles; but we have ",cycleMap[tuple(wizard.toLower,spell.toLower,level)]," cycles.");
+					}else{
+						writeln("unknown: ",wizard,", ",spell,", lv",level,", ",cycles," cycles.");
+					}
+				}
+			}
+		}
+	}
 	import std.exception;
 	enforce(0);
 }
