fd1da5a96a4fe9c5bda9200c736d4544a92aec25
tg <tg@example.com>
2026-09-11 03:37:23 +0200

    Cleanup.

--- source/dagonBackend.d
+++ source/dagonBackend.d
@@ -12,7 +12,7 @@ import core.time: Duration, seconds, MonoTime;
 import lobby;
 import sacobject, sacspell, mrmm, nttData, sacmap, levl, state, form, sacform, controller, network;
 import sxsk : gpuSkinning;
-import renderer,audioBackend,sacrificeBoneRendering;
+import renderer,audioBackend;
 
 final class SacScene: Scene{
 	//OBJAsset aOBJ;
@@ -3168,5 +3168,38 @@ class ShapeCooldown: Owner, Drawable{
 		glDrawElements(GL_TRIANGLES, cast(uint)indices.length * 3, GL_UNSIGNED_INT, cast(void*)0);
 		glBindVertexArray(0);
 		glDepthMask(1);
+	}
+}
+
+class SacrificeBoneMesh: BoneMesh{
+	static bool hideSxmdSeams = false;
+	size_t seamFaceStart = 0;
+	size_t seamFaceCount = 0;
+
+	this(Owner owner){
+		super(owner);
+	}
+
+	override void render(RenderingContext* rc){
+		if(!canRender)
+			return;
+		glBindVertexArray(vao);
+		if(hideSxmdSeams && seamFaceCount != 0){
+			assert(seamFaceStart <= indices.length);
+			assert(seamFaceCount <= indices.length - seamFaceStart);
+
+			if (seamFaceStart != 0)
+				glDrawElements(GL_TRIANGLES, cast(uint)seamFaceStart * 3,
+					GL_UNSIGNED_INT, cast(void*)0);
+
+			const size_t after = seamFaceStart + seamFaceCount;
+			if (after < indices.length)
+				glDrawElements(GL_TRIANGLES, cast(uint)(indices.length - after) * 3,
+					GL_UNSIGNED_INT, cast(void*)(after * 3 * uint.sizeof));
+		}else{
+			glDrawElements(GL_TRIANGLES, cast(uint)indices.length * 3,
+				GL_UNSIGNED_INT, cast(void*)0);
+		}
+		glBindVertexArray(0);
 	}
 }
